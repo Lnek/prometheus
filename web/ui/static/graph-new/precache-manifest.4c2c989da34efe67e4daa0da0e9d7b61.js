32b0c0e166712aac4bc285a109c0d3742d2f871e
eric.wang <yikai.wang01@liulishuo.com>
2020-01-09 11:19:04 +0800

    Fix: ram token timeout

---
+++ web/ui/static/graph-new/precache-manifest.4c2c989da34efe67e4daa0da0e9d7b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99f7379f5c05b388c7fc722c1bb7300",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06374f20e1fb6ec7ae7f",
+    "url": "./static/css/2.3f5e1097.chunk.css"
+  },
+  {
+    "revision": "a4efd5111af24b168d64",
+    "url": "./static/css/main.73fd8119.chunk.css"
+  },
+  {
+    "revision": "06374f20e1fb6ec7ae7f",
+    "url": "./static/js/2.f31f006d.chunk.js"
+  },
+  {
+    "revision": "a4efd5111af24b168d64",
+    "url": "./static/js/main.7e7ee08b.chunk.js"
+  },
+  {
+    "revision": "a6a77ee0a4b2db69e5a6",
+    "url": "./static/js/runtime-main.56ad181a.js"
+  }
+]);
